f18b35ea59de373ec677af6fe85565c3f3336d13
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2023-10-29 23:58:41 +0530

    Update a.c
    After long debugging by ChatGPT
    BingAI finally did it!

--- Expt 8/a.c
+++ Expt 8/a.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
+typedef enum {false, true} bool;
 
 struct node{
 	struct node *lchild;
@@ -19,40 +19,39 @@ struct node * in_succ(struct node *);
 struct node * in_pred(struct node *);
 void inorder(struct node *);
 void preorder(struct node *);
-void postorder (struct node *);
+void search (struct node *, int);
 
 struct node * insert(struct node * root, int ikey){
-	struct node * temp, *par = NULL, *ptr = root;
+	struct node *temp, *par = NULL, *ptr = root;
 	int found = 0;
-	while(ptr!= NULL){
+	while(ptr != NULL){
 		if(ikey == ptr->info){
-			printf("Duplicate\n");
 			found = 1;
 			break;
 		}
 		par = ptr;
 		if(ikey < ptr->info){
-			if(ptr->lthread == false){
+			if(ptr->lthread == false)
 				ptr = ptr->lchild;
 			else break;
 		}
 		else{
 			if(ptr->rthread == false)
-				ptr =ptr->lchild;
+				ptr = ptr->rchild;
 			else break;
 		}
 	}
-	if(!found){
-		if(ptr == NULL){
-			temp = (struct node *)malloc(sizeof(struct node));
-			temp->info = ikey;
-			temp->lthread = true;
-			temp->rthread = true;
-			if(par == NULL){
-				ptr = temp;
-				temp->lchild = NULL;
-				temp->rchild = NULL;
-			}
+	if(found)
+		printf("Duplicate\n");
+	else{
+		temp = (struct node *)malloc(sizeof(struct node));
+		temp->info = ikey;
+		temp->lthread = true;
+		temp->rthread = true;
+		if(par == NULL){
+			root = temp;
+			temp->lchild = NULL;
+			temp->rchild = NULL;
 		}
 		else if(ikey > par->info){
 			temp->rchild = par->rchild;
@@ -71,31 +70,33 @@ struct node * insert(struct node * root, int ikey){
 }
 
 struct node * delete(struct node *root, int dkey){
-	struct node *par = NULL, *ptr = root
+	struct node *par = NULL, *ptr = root;
 	int found = 0;
 	while(ptr!= NULL){
 		if(dkey == ptr->info){
-			printf("Element not found\n");
 			found = 1;
 			break;
 		}
 		par = ptr;
-		if(ikey<ptr->info){
-			if(ptr->lthread == false){
+		if(dkey < ptr->info){
+			if(ptr->lthread == false)
 				ptr = ptr->lchild;
 			else break;
 		}
 		else{
-			if(ptr->rthread = false)
-				ptr =ptr->lchild;
+			if(ptr->rthread == false)
+				ptr = ptr->rchild;
 			else break;
 		}
 	}
-	if(!found)
+	if(found == 0)
+		printf("Element not found\n");
 	else if(ptr->lthread == false && ptr->rthread == false)
 		root = case_c(root, par, ptr);
-	else if (ptr->lthread == false || ptr->rthread == false)
-		root = case_c(root, par, ptr);
+	else if (ptr->lthread == false && ptr->rthread == true)
+        root = case_b(root, par, ptr);
+    else if (ptr->lthread == true && ptr->rthread == false)
+        root = case_b(root, par, ptr);
 	else root = case_a(root, par, ptr);
 	return root;
 }
@@ -112,6 +113,7 @@ struct node * case_a(struct node *root, struct node *par, struct node *ptr){
 		par->rchild = ptr->rchild;
 	}
 	free(ptr);
+	printf("Deleted the element\n");
 	return root;
 }
 
@@ -126,15 +128,16 @@ struct node * case_b(struct node *root, struct node *par, struct node *ptr){
 	else if(ptr == par->lchild)
 		par->lchild = child;
 	else par->rchild = child;
-	a = in_succ(ptr;
+	s = in_succ(ptr);
 	p = in_pred(ptr);
 	if(ptr->lthread == false)
-		p->rchild = a;
+		p->rchild = s;
 	else{
 		if(ptr->rthread == false)
-			a->lchild = p;
+			s->lchild = p;
 	}
 	free(ptr);
+	printf("Deleted the element\n");
 	return root;
 }
 
@@ -158,8 +161,8 @@ struct node * in_succ(struct node * ptr){
 		ptr = ptr->rchild;
 		while(ptr->lthread == false)
 			ptr = ptr->lchild;
+		return ptr;
 	}
-	return ptr;
 }
 
 struct node * in_pred(struct node * ptr){
@@ -169,8 +172,8 @@ struct node * in_pred(struct node * ptr){
 		ptr = ptr->lchild;
 		while(ptr->rthread == false)
 			ptr = ptr->rchild;
+		return ptr;
 	}
-	return ptr;
 }
 
 void inorder(struct node * root){
@@ -189,7 +192,7 @@ void inorder(struct node * root){
 
 void preorder(struct node * root){
 	struct node * ptr = root;
-	if(ptr == NULL){
+	if(root == NULL){
 		printf("Empty\n");
 		return;
 	}
@@ -198,88 +201,73 @@ void preorder(struct node * root){
 		if(ptr->lthread == false)
 			ptr = ptr->lchild;
 		else if (ptr->rthread == false)
-			ptr = ptr->rthread;
-		else{
-			while(ptr!=NULL && ptr->rthread == false)
-				ptr = ptr->rchild;
-			if(ptr != NULL)
-				ptr = ptr->rchild;
-		}
-	}
-}
-
-void postorder(struct node * root){
-	struct node * ptr = root;
-	if(ptr == NULL){
-		printf("Empty\n");
-		return;
-	}
-	while(ptr != NULL){
-		printf(" %d", ptr->info);
-		if(ptr->lthread == false)
-			ptr = ptr->lchild;
-		else if (ptr->rthread == false)
-			ptr = ptr->rthread;
-		else{
-			while(ptr!=NULL && ptr->rthread == false)
-				ptr = ptr->rchild;
-			if(ptr != NULL)
-				ptr = ptr->rchild;
-		}
-	}
-}
-
-struct node* search(struct node* ptr, int skey){
-    while(ptr->lthread == true && ptr->rthread == true){
-		if(ptr->info == skey)
-			return ptr;
-		else if(ptr->info > skey)
 			ptr = ptr->rchild;
-		else ptr = ptr->lchild;
+		else{
+			while(ptr!=NULL && ptr->rthread == true)
+				ptr = ptr->rchild;
+			if(ptr != NULL)
+				ptr = ptr->rchild;
+		}
+	}
+}
+
+void search(struct node* ptr, int skey){
+    while(ptr->lthread == false || ptr->rthread == false){
+		if(ptr->info == skey){
+			printf("Element found\n");
+			return;
+		}
+		else if(ptr->info > skey && ptr->lthread == false)
+			ptr = ptr->lchild;
+		else if (ptr->info < skey && ptr->rthread == false)
+            ptr = ptr->rchild;
 	}
 	printf("Element not found\n");
-	return NULL;
 }
 
 int main(){
 	struct node *root = NULL;
-	int s;
+	int s, key, n;
 	char c;
-	printf("Do you want to insert many elements? y/n: ");
-	scanf("%c", &c);
-	if(c == 'y')
-	// add for loop
+	printf("Do you want to insert many elements? (y/n): ");
+	scanf("%c", &c);getchar();
+	if(c == 'y'){
+		printf("Enter number of elements to insert: ");
+		scanf("%d", &n);
+		for(int i = 0; i < n; i++){
+			printf("Insert: ");
+			scanf("%d", &key);
+			root = insert(root, key);
+		}
+	}
 	do{
 		printf("\n1. Insertion of a new element\n"
 				"2. Deletion of an existing element\n"
 				"3. Searching for a given element\n"
-				"4. Perform inorder, preorder and postorder traversals\n"
+				"4. Perform inorder and preorder traversals\n"
 				"5. Exit\n");
 		printf("Enter your option: ");
 		scanf("%d", &s);
 		switch(s){
 			case 1: printf("Insert: ");
-					scanf("%d", &k);
-					root = insert(root, k);
+					scanf("%d", &key);
+					root = insert(root, key);
 					break;
 			case 2: printf("Delete: ");
-					scanf("%d", &k);
-					root = delete(root, k);
+					scanf("%d", &key);
+					root = delete(root, key);
 					break;
 			case 3: printf("Enter the element: ");
-					scanf("%d", &c);
-					seek = search(root, c);
-					if(seek == NULL)
-						printf("Element not found\n");
-					else printf("Element found\n");
+					scanf("%d", &key);
+					search(root, key);
 					break;
 			case 4: printf("Inorder: "); inorder(root);
 					printf("\nPreorder: "); preorder(root);
-					printf("\nPostorder: "); postorder(root);
 					break;
-			
 			case 5: break;
 			default: printf("Invalid input\n");
 		}
-	}while(s != 6);
+	}while(s != 5);
+
+	return 0;
 }
