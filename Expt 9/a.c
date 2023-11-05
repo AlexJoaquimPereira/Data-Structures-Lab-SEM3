a7e6a7fcaf7924132e9b257841c9d308acd62d65
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2023-11-05 21:33:37 +0530

    Change a.c
    Edited functions, whitespace, main body to make it look similar to my aesthetic

--- Expt 9/a.c
+++ Expt 9/a.c
@@ -3,7 +3,7 @@
 #define TRUE 1
 #define FALSE 0
 
-struct node {
+struct node{
     struct node *lchild;
     int info;
     struct node *rchild;
@@ -26,21 +26,17 @@ struct node *LeftRotation(struct node *pptr){
     return aptr;
 }
 
-
 struct node *insert_rightbalance(struct node *pptr){
     struct node *aptr, *bptr;
     aptr = pptr->lchild;
-    if (aptr->balance == -1)
-    {
+    if (aptr->balance == -1){
         pptr->balance = 0;
         aptr->balance = 0;
         pptr = RightRotation(pptr);
     }
-    else
-    {
+    else{
         bptr = aptr->rchild;
-        switch (bptr->balance)
-        {
+        switch (bptr->balance){
             case -1:
                 pptr->balance = 1;
                 aptr->balance = 0;
@@ -63,17 +59,14 @@ struct node *insert_rightbalance(struct node *pptr){
 struct node *insert_leftbalance(struct node *pptr){
     struct node *aptr, *bptr;
     aptr = pptr->lchild;
-    if (aptr->balance == 1)
-    {
+    if (aptr->balance == 1){
         pptr->balance = 0;
         aptr->balance = 0;
         pptr = RightRotation(pptr);
     }
-    else
-    {
+    else{
         bptr = aptr->rchild;
-        switch (bptr->balance)
-        {
+        switch (bptr->balance){
             case -1:
                 pptr->balance = 0;
                 aptr->balance = 1;
@@ -94,8 +87,7 @@ struct node *insert_leftbalance(struct node *pptr){
 }
 
 struct node *insert_left_check(struct node *pptr, int *ptaller){
-    switch (pptr->balance)
-    {
+    switch (pptr->balance){
         case 0:
             pptr->balance = 1;
             break;
@@ -111,8 +103,7 @@ struct node *insert_left_check(struct node *pptr, int *ptaller){
 }
 
 struct node *insert_right_check(struct node *pptr, int *ptaller){
-    switch (pptr->balance)
-    {
+    switch (pptr->balance){
         case 0:
             pptr->balance = -1;
             break;
@@ -129,9 +120,7 @@ struct node *insert_right_check(struct node *pptr, int *ptaller){
 
 struct node *insert(struct node *pptr, int ikey){
     static int taller;
-
-    if (pptr == NULL)
-    {
+    if (pptr == NULL){
         pptr = (struct node *)malloc(sizeof(struct node));
         pptr->info = ikey;
         pptr->lchild = NULL;
@@ -139,27 +128,22 @@ struct node *insert(struct node *pptr, int ikey){
         pptr->balance = 0;
         taller = TRUE;
     }
-    else if (ikey < pptr->info)
-    {
+    else if (ikey < pptr->info){
         pptr->lchild = insert(pptr->lchild, ikey);
         if (taller == TRUE)
             pptr = insert_left_check(pptr, &taller);
     }
-    else if (ikey > pptr->info)
-    {
+    else if (ikey > pptr->info){
         pptr->rchild = insert(pptr->rchild, ikey);
         if (taller == TRUE)
             pptr = insert_right_check(pptr, &taller);
     }
-    else
-    {
+    else{
         printf("Duplicate key\n");
         taller = FALSE;
     }
     return pptr;
 }
-
-
 
 struct node*del_rightbalance(struct node *pptr,int *pshorter){
     struct node *bptr,*aptr;
@@ -214,7 +198,6 @@ struct node *del_left_check(struct node *pptr,int *pshorter){
     return pptr;
 }
 
-
 struct node* del_leftbalance(struct node *pptr, int *pshorter){
     struct node *bptr, *aptr;
     aptr = pptr->rchild;
@@ -224,14 +207,14 @@ struct node* del_leftbalance(struct node *pptr, int *pshorter){
         *pshorter = FALSE;
         pptr = LeftRotation(pptr);
     } 
-    else if (aptr->balance == -1) {
+    else if (aptr->balance == -1){
         pptr->balance = 0;
         aptr->balance = 0;
         pptr = LeftRotation(pptr);
     } 
     else {
         bptr = aptr->lchild;
-        switch (bptr->balance) {
+        switch (bptr->balance){
             case 0:
                 pptr->balance = 0;
                 aptr->balance = 0;
@@ -253,7 +236,7 @@ struct node* del_leftbalance(struct node *pptr, int *pshorter){
 }
 
 struct node *del_right_check(struct node *pptr, int *pshorter){
-    switch (pptr->balance) {
+    switch (pptr->balance){
         case 0:
             pptr->balance = 1;
             *pshorter = FALSE;
@@ -268,9 +251,7 @@ struct node *del_right_check(struct node *pptr, int *pshorter){
     return pptr;
 }
 
-
-
-struct node *del(struct node * pptr,int dkey){
+struct node *delete(struct node * pptr,int dkey){
     struct node * tmp, * succ;
     static int shorter;
     if(pptr==NULL){
@@ -278,7 +259,6 @@ struct node *del(struct node * pptr,int dkey){
         shorter=FALSE;
         return pptr;
     }
-
     if(dkey<pptr->info){
         pptr->lchild=del(pptr->lchild,dkey);
         if(shorter==TRUE){
@@ -291,7 +271,6 @@ struct node *del(struct node * pptr,int dkey){
             pptr=del_right_check(pptr,&shorter);
         }
     }
-
     else{
         if(pptr->lchild!=NULL && pptr->lchild!=NULL){
             succ=pptr->lchild;
@@ -317,37 +296,33 @@ struct node *del(struct node * pptr,int dkey){
     return pptr;
 }
 
-void search(struct node * ptr,int skey){
+void search(struct node *ptr, int skey){
     static int found=0;
-    if(ptr==NULL){printf("Key not found\n");return;}
+    if(ptr == NULL){
+        printf("Element not found\n");
+        return;
+    }
     else if(skey<ptr->info)
         search(ptr->lchild,skey);
     else if(skey>ptr->info)
         search(ptr->rchild,skey);
-    else
-        found=1;
-    
-    if(found==0){
+    else found=1;   
+    if(found == 0)
         printf("Element not found\n");
-    }
-    else{
-        printf("found element\n");
-    }
+    else printf("Element found\n");
 }
 
-void min(struct node *ptr){
-    if(ptr!=NULL){
+void minimum(struct node *ptr){
+    if(ptr!=NULL)
         while(ptr->lchild!=NULL)
             ptr=ptr->lchild;
-    }
     printf("Minimum element is : %d",ptr->info);
 }
 
-void max(struct node *ptr){
-    if(ptr!=NULL){
+void maximum(struct node *ptr){
+    if(ptr!=NULL)
         while(ptr->rchild!=NULL)
             ptr=ptr->rchild;
-    }
     printf("Maximum element is : %d",ptr->info);
 }
 
@@ -355,38 +330,31 @@ int main(){
     struct node *root = NULL;
     int s, key;
     do{
-        printf("1. Insert\n");
-        printf("2. Delete\n");
-        printf("3. Search\n");
-        printf("4. min\n");
-        printf("5. max\n");
-        printf("6. Exit\n");
-        printf("Enter your choice: ");
+        printf("\n1. Insertion of a new element\n"
+				"2. Deletion of an existing element\n"
+				"3. Searching for a given element\n"
+				"4. Find minimum and maximum element\n"
+				"5. Exit\n");
+		printf("Enter your option: ");
         scanf("%d", &s);
         switch(s){
-            case 1:
-                printf("Enter key to insert: ");
-                scanf("%d", &key);
-                root = insert(root, key);
-                break;
-            case 2:
-                printf("Enter key to delete: ");
-                scanf("%d", &key);
-                root = del(root, key);
-                break;
-            case 3:
-                printf("Enter key to search: ");
-                scanf("%d", &key);
-                search(root, key);
-                break;
-            case 4:
-                min(root);
-                break;
-            case 5:
-                max(root);
-                break;
+            case 1: printf("Insert: ");
+                    scanf("%d", &key);
+                    root = insert(root, key);
+                    break;
+            case 2: printf("Delete: ");
+                    scanf("%d", &key);
+                    root = delete(root, key);
+                    break;
+            case 3: printf("Enter the element: ");
+                    scanf("%d", &key);
+                    search(root, key);
+                    break;
+            case 4: minimum(key);
+                    maximum(key);
+                    break;
             case 6: break;
-            default: printf("Invalid choice\n");
+            default: printf("Invalid input\n");
         }
     }while(s != 6);
     return 0;
