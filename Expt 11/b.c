8978ae8b4a8275953fdc97c286d9532de8f40920
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2023-11-09 17:31:42 +0530

    Update b.c
    Added actual hash functions, unserstood something about hashing

--- Expt 11/b.c
+++ Expt 11/b.c
@@ -3,6 +3,8 @@
 #include<string.h>
 #define MAX 100
 enum type_of_record {EMPTY, DELETED, OCCUPIED};
+int ctech;
+
 struct employee{
 	int empid;
 	char empname[20];
@@ -12,7 +14,52 @@ struct employee{
 struct Record{
 	struct employee info;
 	enum type_of_record status;
-};
+}table[MAX];
+
+int hash(int key){
+	return key % MAX;
+}
+
+int collision_resolution(int h){
+	//Enter switch for diff collision tech and return location;
+	
+}
+
+int search(int key){
+	int i, h, location;
+	h = hask(key);
+	location = h;
+	for(i = 1; i < MAX; i++){
+		if(table[location].status == EMPTY)
+			return -1;
+		if(table[location].info.empid == key)
+			return location;
+		location = collision_resolution(h);
+	}
+	return -1;
+}
+
+void insert(struct employee emprec){
+	int i, h, location;
+	h = hash(emprec.empid);
+	location = h;
+	for (i = 1; i < MAX; i++){
+		if (table[location].status == EMPTY || table[location].status = DELETED){
+			table[location].info = emprec;
+			table[location].status = OCCUPIED;
+			return;
+		}
+		location = collision_resolution(h);
+	}
+	printf("Table overflow\n");
+}
+
+void delete(int key){
+	int location = search(key);
+	if(location == -1)
+		printf("Not found\n");
+	else table[location].status = DELETED;
+}
 
 int main(){
 	struct employee *arr;
@@ -23,7 +70,7 @@ int main(){
 	printf("Enter the employee details:\n");
 	for(int i = 0; i < n; i++){
 		printf("Employee %d\n:", i+1);
-		printf("ID: "):
+		printf("ID: ");
 		scanf("%d", &arr[i].empid);
 		printf("Name: ");
 		scanf("%s", arr[i].empname);
@@ -32,25 +79,15 @@ int main(){
 		printf("-----------------------\n");
 	}
 	do{
-		printf("1. Insert a record\n");
-		printf("2. Binary search\n");
+		printf("\n");
+		printf("\n");
 		printf("3. Exit\n");
 		printf("Enter your option: ");
 		scanf("%d", &s);
 		switch(s){
-			case 1: printf("Enter the ID: ");
-					scanf("%d", &item);
-					item = linear_search(arr, n, item);
-					if(item == -1)
-						printf("Item not found\n");
-					else printf("Item found at %dth location", item);
+			case 1: 
 					break;
-			case 2: printf("Enter the ID: ");
-					scanf("%d", &item);
-					item = binary_search(arr, n, item);
-					if(item == -1)
-						printf("Item not found\n");
-					else printf("Item found at %dth location", item);
+			case 2: 
 					break;
 			case 3: break;
 			case 4: printf("Invalid input\n");
